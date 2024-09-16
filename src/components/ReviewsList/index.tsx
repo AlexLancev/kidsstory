a9dc33d79e8b92c99ced6f801eb4efd251e7bd07
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2024-09-16 23:12:14 +0500

    add fix component reviewList

--- src/components/ReviewsList/index.tsx
+++ src/components/ReviewsList/index.tsx
@@ -34,49 +34,60 @@ export const ReviewsList: React.FC = () => {
     return null;
   }
 
-  return (
-    reviewsArray && (
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={2}
-        pagination={{ clickable: true }}
-        loop={true}
-      >
-        {reviewsArray.map((item: ReviewsType, index: number) => (
-          <SwiperSlide
-            key={item._id || index}
-            className={styles.reviewsListItem}
-          >
-            <div className={styles.reviewsCurrent}>
-              <div className={styles.reviewsPerson}>
-                <img
-                  className={styles.reviewsPersonImg}
-                  width={60}
-                  height={60}
-                  src={item.image}
-                  alt=''
-                  aria-hidden
-                />
-                <div className={styles.person}>
-                  <b className={styles.reviewsName}>{item.whoseReview}</b>
-                  <span className={styles.reviewsSity}>{item.sity}</span>
-                </div>
-              </div>
-              <p className={styles.reviewsDescription}>{item.description}</p>
-              <button
-                className={styles.reviewsBtn}
-                type='button'
-                onClick={() => handleClick(item)}
-              >
-                Прочитать весь отзыв
-              </button>
-            </div>
-          </SwiperSlide>
-        ))}
+  const renderReviewItem = (item: ReviewsType) => {
+    return (
+      <li className={styles.reviewsCurrent} key={item._id}>
+        <div className={styles.reviewsPerson}>
+          <img
+            className={styles.reviewsPersonImg}
+            width={60}
+            height={60}
+            src={item.image}
+            alt=''
+            aria-hidden
+          />
+          <div className={styles.person}>
+            <strong className={styles.reviewsName}>{item.whoseReview}</strong>
+            <span className={styles.reviewsSity}>{item.sity}</span>
+          </div>
+        </div>
+        <p className={styles.reviewsDescription}>{item.description}</p>
+        <button
+          className={styles.reviewsBtn}
+          type='button'
+          onClick={() => handleClick(item)}
+        >
+          Прочитать весь отзыв
+        </button>
         {isModalVisible && currentReview && (
           <Modal review={currentReview} onClose={handleCloseModal} />
         )}
-      </Swiper>
-    )
+      </li>
+    );
+  };
+
+  return isLoading ? (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={2}
+      pagination={{ clickable: true }}
+      loop
+    >
+      {reviewsArray.map((item: ReviewsType, index: number) => (
+        <SwiperSlide key={item._id || index} className={styles.reviewsListItem}>
+          {renderReviewItem(item)}
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  ) : (
+    <ul className={styles.reviewsList}>
+      {isLoading || !reviewsArray
+        ? Array.from({ length: reviewsArray.length }).map(
+            (_, index: number) => {
+              return <i key={index}></i>;
+            },
+          )
+        : reviewsArray.map((item) => renderReviewItem(item))}
+    </ul>
   );
 };
