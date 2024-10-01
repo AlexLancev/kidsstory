abe5e427ea75a224aee6ac979136abd4adfed2b8
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2024-10-01 22:22:54 +0500

    add fix component servisePageId

--- src/components/ServicesList/ServicesPageId/index.tsx
+++ src/components/ServicesList/ServicesPageId/index.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import { FC, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import { AppDispatch, RootState } from 'store/index';
 import { getServiceId } from 'store/service/serviceSlice';
 
-// import styles from './ServicesPageId.module.css';
+import styles from './ServicesPageId.module.css';
+import { ServiceIdType } from 'types/index';
 
-export const ServicesPageId: React.FC = () => {
+export const ServicesPageId: FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { id } = useParams();
   const { serviceId, isLoading } = useSelector(
     (state: RootState) => state.serviceId,
   );
-  React.useEffect(() => {
+
+  useEffect(() => {
     if (id) {
       dispatch(getServiceId(id));
     }
@@ -23,5 +25,32 @@ export const ServicesPageId: React.FC = () => {
     return null;
   }
 
-  return <h1>hello</h1>;
+  return (
+    <>
+      {serviceId.map((el: ServiceIdType) => {
+        const { imageBg, icon, title, description } = el;
+        return (
+          <div key={el._id} className={styles.servicesPageId}>
+            <div
+              className={styles.servicesPageIdHero}
+              style={{ backgroundImage: `url(${imageBg})` }}
+            >
+              <div className={styles.servicesPageIdType}>
+                <span
+                  className={styles.servicesPageIdIcon}
+                  style={{ backgroundImage: `url(${icon})` }}
+                ></span>
+                <strong className={styles.servicesPageIdHead}>{title}</strong>
+              </div>
+            </div>
+            <div
+              className={styles.servicesPageIdDescription}
+              dangerouslySetInnerHTML={{ __html: description }}
+            ></div>
+          </div>
+        );
+      })}
+    </>
+  );
+  
 };
--- src/types/api/serviceId.ts
+++ src/types/api/serviceId.ts
@@ -1,4 +1,5 @@
 export type ServiceIdType = {
+  map(arg0: (el: ServiceIdType) => import("react/jsx-runtime").JSX.Element): unknown;
   _id: string;
   icon: string;
   imagePromo: string;
