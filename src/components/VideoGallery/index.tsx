ee1bc6813b810a47ace020fb7f2d9a9e16b5eaec
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2024-11-21 21:20:45 +0500

    фикс VideoGallery слайдера

--- src/components/VideoGallery/index.tsx
+++ src/components/VideoGallery/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
+import React, { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Pagination, Autoplay } from 'swiper';
+import { videoGalleryArray } from 'constans/videoGalleryArray';
 
 SwiperCore.use([Pagination, Autoplay]);
-
-import { videoGalleryArray } from 'constans/videoGalleryArray';
 
 import Play from './img/video-gallery/play.svg?react';
 
@@ -15,26 +13,57 @@ import 'swiper/components/pagination/pagination.min.css';
 import styles from './VideoGallery.module.css';
 
 export const VideoGallery: React.FC = () => {
+  const [activeVideo, setActiveVideo] = useState<number | null>(null);
+  const swiperRef = useRef<SwiperCore | null>(null);
+
+  const handleSlideChange = (swiper: SwiperCore) => {
+    if (activeVideo !== swiper.realIndex) {
+      setActiveVideo(null);
+      swiper.autoplay.start();
+    }
+  };
+
+  const handlePlayVideo = (index: number) => {
+    setActiveVideo(index);
+    if (swiperRef.current) {
+      swiperRef.current.autoplay.stop();
+    }
+  };
+
   return (
     videoGalleryArray && (
       <div className={`${styles.slide} slide-page`}>
         <Swiper
+          onSwiper={(swiper) => (swiperRef.current = swiper)}
           spaceBetween={0}
           slidesPerView={3}
           pagination={{ clickable: true }}
           loop={true}
           centeredSlides={true}
+          onSlideChange={handleSlideChange}
           // autoplay={{ delay: 7000, disableOnInteraction: false }}
         >
           {videoGalleryArray.map((item, index: number) => (
             <SwiperSlide key={item.id || index}>
-              <ReactPlayer
-                controls={true}
-                light={item.urlImage}
-                url={item.urlVideo}
-                playIcon={<Play width={96} />}
-                playing
-              />
+              {activeVideo === index ? (
+                <iframe
+                  src={item.urlVideo}
+                  title='YouTube video player'
+                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                ></iframe>
+              ) : (
+                <>
+                  <img src={item.urlImage} alt='' aria-hidden />
+                  <button
+                    type='button'
+                    onClick={() => handlePlayVideo(index)}
+                    title='Воспроизвести видео'
+                  >
+                    <Play />
+                    <span className='visually-hidden'>Воспроизвести видео</span>
+                  </button>
+                </>
+              )}
             </SwiperSlide>
           ))}
         </Swiper>
--- src/constans/videoGalleryArray.ts
+++ src/constans/videoGalleryArray.ts
@@ -4,36 +4,36 @@ export const videoGalleryArray: VideoGalleryType[] = [
   {
     id: '1',
     urlImage: 'img/video-gallery/item-1.webp',
-    urlVideo: 'https://youtu.be/AtYQFfiV_34?si=lPzeP1cP5g90S2Cj',
+    urlVideo: 'https://www.youtube.com/embed/AtYQFfiV_34',
   },
   {
     id: '2',
     urlImage: 'img/video-gallery/item-2.webp',
-    urlVideo: 'https://youtu.be/Xh-973JTckY?si=J1RZDMd6pK-tb_Sk',
+    urlVideo: 'https://www.youtube.com/embed/Xh-973JTckY',
   },
   {
     id: '3',
     urlImage: 'img/video-gallery/item-3.webp',
-    urlVideo: 'https://youtu.be/xwt20xE7ky0?si=-Mj98oBSTSr2zyvZ',
+    urlVideo: 'https://www.youtube.com/embed/xwt20xE7ky0',
   },
   {
     id: '4',
     urlImage: 'img/video-gallery/item-4.webp',
-    urlVideo: 'https://youtu.be/kS2jhSL6vRo?si=0AyAq2CCtWk4OkxU',
+    urlVideo: 'https://www.youtube.com/embed/kS2jhSL6vRo',
   },
   {
     id: '5',
     urlImage: 'img/video-gallery/item-5.webp',
-    urlVideo: 'https://youtu.be/kpORYBd_G4k?si=PPHOb90c1VpWj0ZT',
+    urlVideo: 'https://www.youtube.com/embed/kpORYBd_G4k',
   },
   {
     id: '6',
     urlImage: 'img/video-gallery/item-6.webp',
-    urlVideo: 'https://youtu.be/lM94uaYhJAA?si=C4ETqSjcR9dhw19l',
+    urlVideo: 'https://www.youtube.com/embed/lM94uaYhJAA',
   },
   {
     id: '7',
     urlImage: 'img/video-gallery/item-7.webp',
-    urlVideo: 'https://youtu.be/ZYw4GzpOsko?si=V_cfKsmGMLn1bykI',
+    urlVideo: 'https://www.youtube.com/embed/ZYw4GzpOsko',
   },
 ];
