e0f3c19cdd4e50516f7150d2fe82473c28a406e5
Alex Vavulo <aleksei.lancevv@gmail.com>
2024-12-28 18:40:11 +0500

    исправление вложенности

--- src/pages/SchedulePage/ScheduleGroupPage/ScheduleGroupSection/index.tsx
+++ src/pages/SchedulePage/ScheduleGroupPage/ScheduleGroupSection/index.tsx
@@ -9,43 +9,39 @@ export const ScheduleGroupSection: FC = () => {
 
   return (
     <section className={styles.scheduleGroupSection}>
-      <div className='container'>
-        <h1 className={styles.scheduleGroupSectionTitle}>Расписание групп</h1>
-        <div>
-          {scheduleGroupData.map((group) => (
-            <div className={styles.scheduleBox} key={group.groupName}>
-              <h2 className={styles.scheduleHead}>{group.groupName}</h2>
-              <table className={styles.scheduleTable}>
-                <thead>
-                  <tr>
-                    <th className={styles.dailyDay}>День недели</th>
-                    <th className={styles.dailyTime}>Время</th>
-                    <th className={styles.dailyLesson}>Занятие</th>
+      <h1 className={styles.scheduleGroupSectionTitle}>Расписание групп</h1>
+      {scheduleGroupData.map((group) => (
+        <div className={styles.scheduleBox} key={group.groupName}>
+          <h2 className={styles.scheduleHead}>{group.groupName}</h2>
+          <table className={styles.scheduleTable}>
+            <thead>
+              <tr>
+                <th className={styles.dailyDay}>День недели</th>
+                <th className={styles.dailyTime}>Время</th>
+                <th className={styles.dailyLesson}>Занятие</th>
+              </tr>
+            </thead>
+            <tbody>
+              {group.scheduleData.map((daySchedule) =>
+                daySchedule.schedule.map((item, index) => (
+                  <tr key={item.id}>
+                    {index === 0 && (
+                      <td
+                        rowSpan={daySchedule.schedule.length}
+                        className={styles.dailyDayCell}
+                      >
+                        {daySchedule.day}
+                      </td>
+                    )}
+                    <td>{item.time}</td>
+                    <td className={styles.subject}>{item.subject}</td>
                   </tr>
-                </thead>
-                <tbody>
-                  {group.scheduleData.map((daySchedule) =>
-                    daySchedule.schedule.map((item, index) => (
-                      <tr key={item.id}>
-                        {index === 0 && (
-                          <td
-                            rowSpan={daySchedule.schedule.length}
-                            className={styles.dailyDayCell}
-                          >
-                            {daySchedule.day}
-                          </td>
-                        )}
-                        <td>{item.time}</td>
-                        <td className={styles.subject}>{item.subject}</td>
-                      </tr>
-                    )),
-                  )}
-                </tbody>
-              </table>
-            </div>
-          ))}
+                )),
+              )}
+            </tbody>
+          </table>
         </div>
-      </div>
+      ))}
     </section>
   );
 };
